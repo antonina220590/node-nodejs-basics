90a6d9f9797657a9bda69082913cebbfdf42be37
antonina220590 <tonya2290@gmail.com>
2025-04-28 09:23:22 +0300

    feat: add checking if the file exist in the directory

--- src/fs/create.js
+++ src/fs/create.js
@@ -3,25 +3,37 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 const __currentFilePath = fileURLToPath(import.meta.url);
-console.log("filename", __currentFilePath);
 const __currentFolderPath = path.dirname(__currentFilePath);
-console.log("dirname", __currentFolderPath);
-
 const folder_name = "files";
 const file_name = "fresh.txt";
+const file_content = "I am fresh and young";
+const error_message = "FS operation failed";
 
 const targetFolderPath = path.join(__currentFolderPath, folder_name);
-console.log("folderPath", targetFolderPath);
 const targetFilePath = path.join(targetFolderPath, file_name);
-console.log("filePath", targetFilePath);
 
 const create = async () => {
   try {
     await mkdir(targetFolderPath, { recursive: true });
-    await writeFile(targetFilePath, "");
+    await writeFile(targetFilePath, file_content, {
+      flag: "wx",
+      encoding: "utf8",
+    });
+    console.log(
+      `File ${file_name} created successfully in ${targetFolderPath}`
+    );
   } catch (error) {
-    console.error(`Creating file error: ${error.message}`);
+    if (error.code === "EEXIST") {
+      throw new Error(error_message);
+    } else {
+      console.error(`An unexpected error: ${error.message}`);
+      throw error;
+    }
   }
 };
 
-await create();
+try {
+  await create();
+} catch (error) {
+  console.error(error.message);
+}
