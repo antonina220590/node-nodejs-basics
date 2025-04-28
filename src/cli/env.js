ee132cc2e77f1dfc5931499826cebd1fa43710a1
antonina220590 <tonya2290@gmail.com>
2025-04-28 14:48:11 +0300

    feat: add function for env.js file

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
+const prefix = "RSS_";
 const parseEnv = () => {
-    // Write your code here 
+  const envObj = process.env;
+  const envKeys = Object.keys(envObj);
+  const filteredKeys = envKeys.filter((key) => key.startsWith(prefix));
+  const pairs = filteredKeys.map((key) => {
+    const value = envObj[key];
+    return `${key}=${value}`;
+  });
+  const output = pairs.join("; ");
+  console.log(output);
 };
 
 parseEnv();
