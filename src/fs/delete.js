6afefabf8333d7841d77d4b1a3fcb51e604606d1
antonina220590 <tonya2290@gmail.com>
2025-04-28 13:01:56 +0300

    feat: add function to delete.js file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,31 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __currentFilePath = fileURLToPath(import.meta.url);
+const __currentFolderPath = path.dirname(__currentFilePath);
+const folder_name = "files";
+const target_file = "fileToRemove.txt";
+const error_message = "FS operation failed";
+
+const folder_path = path.join(__currentFolderPath, folder_name);
+const target_file_path = path.join(folder_path, target_file);
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await fs.unlink(target_file_path);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error(error_message);
+    } else {
+      console.error(`Error deleting file: ${error.message}`);
+      throw error;
+    }
+  }
 };
 
-await remove();
+try {
+  await remove();
+} catch (error) {
+  console.error(error.message);
+}
