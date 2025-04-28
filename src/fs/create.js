a0d3553e79f6d6995aaa949d7d3f3377829abbe1
antonina220590 <tonya2290@gmail.com>
2025-04-28 09:02:13 +0300

    feat: create empty file fresh.txt in the directory

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import { writeFile, mkdir } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __currentFilePath = fileURLToPath(import.meta.url);
+console.log("filename", __currentFilePath);
+const __currentFolderPath = path.dirname(__currentFilePath);
+console.log("dirname", __currentFolderPath);
+
+const folder_name = "files";
+const file_name = "fresh.txt";
+
+const targetFolderPath = path.join(__currentFolderPath, folder_name);
+console.log("folderPath", targetFolderPath);
+const targetFilePath = path.join(targetFolderPath, file_name);
+console.log("filePath", targetFilePath);
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await mkdir(targetFolderPath, { recursive: true });
+    await writeFile(targetFilePath, "");
+  } catch (error) {
+    console.error(`Creating file error: ${error.message}`);
+  }
 };
 
 await create();
