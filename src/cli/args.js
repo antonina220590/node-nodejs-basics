4f236289e980ad25b992ae601dd6eb970cba0517
antonina220590 <tonya2290@gmail.com>
2025-04-28 15:04:36 +0300

    feat: add function to args.js file

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const pairs = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    const prefixKey = args[i];
+    const value = args[i + 1];
+
+    if (prefixKey && prefixKey.startsWith("--")) {
+      const key = prefixKey.slice(2);
+      pairs.push(`${key} is ${value}`);
+    }
+  }
+  const output = pairs.join(", ");
+  console.log(output);
 };
 
 parseArgs();
